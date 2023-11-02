09a1ca43c9e65c66860068adca071815383cf290
Eliot Fagnon <efagnon2002@gmail.com>
2023-11-02 01:44:23 +0100

    feat: Add animation to stepper bar

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,23 +1,29 @@
 'use client';
 
 import AuthStepper from '@/components/auth-stepper/auth-stepper';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@/components/button/button';
 import Link from 'next/link';
 import AccountTypeStep from '@/components/auth/signup-steps/account-type-step';
 
 function Page(): React.JSX.Element {
-    const [step, setStep] = useState<number>(0);
+    const [step, setStep] = useState<number>(1);
 
     const steps: React.JSX.Element[] = [<AccountTypeStep key={0} />];
 
     const nextStep = () => {
-        setStep((step) => ++step);
+        setStep((step) => {
+            if (step <= 4) {
+                return step + 1;
+            } else {
+                return step;
+            }
+        });
     };
 
     return (
         <div className="flex flex-col items-center">
-            <AuthStepper />
+            <AuthStepper step={step} />
 
             <h1 className="my-12 text-5xl font-bold">
                 Let's <span className="text-primary">sign</span> you{' '}
@@ -26,7 +32,9 @@ function Page(): React.JSX.Element {
 
             {steps[0]}
 
-            <Button className={'mt-8'}>Next</Button>
+            <Button className={'mt-8'} onClick={nextStep}>
+                Next
+            </Button>
 
             <p className={'my-12'}>
                 Already have an account?{' '}
--- src/components/auth-stepper/auth-stepper.tsx
+++ src/components/auth-stepper/auth-stepper.tsx
@@ -1,21 +1,70 @@
-import React from 'react';
+'use client';
 
-export default function AuthStepper(): React.JSX.Element {
+import cn from '@/lib/utils/cn';
+import React from 'react';
+import { motion } from 'framer-motion';
+
+export default function AuthStepper({ step }: { step: number }): React.JSX.Element {
     return (
         <div className="flex items-center justify-center">
             <div className="bg-primary flex h-14 w-14 items-center justify-center rounded-full text-xl text-white">
                 1
             </div>
-            <div className="w-12 border-[3px] border-black"></div>
-            <div className="flex h-14 w-14 items-center justify-center rounded-full bg-black text-xl text-white">
+            <div className={cn('h-[4px] w-12 bg-black')}>
+                <motion.div
+                    className={'bg-primary h-[4px]'}
+                    animate={{
+                        width: step > 1 ? '100%' : 0,
+                    }}
+                    transition={{
+                        duration: 0.2,
+                    }}
+                ></motion.div>
+            </div>
+            <div
+                className={cn(
+                    'flex h-14 w-14 items-center justify-center rounded-full text-xl text-white',
+                    `${step >= 2 ? 'bg-primary' : 'bg-black'}`,
+                )}
+            >
                 2
             </div>
-            <div className="w-12 border-[3px] border-black"></div>
-            <div className="flex h-14 w-14 items-center justify-center rounded-full bg-black text-xl text-white">
+            <div className={cn('h-[4px] w-12 bg-black')}>
+                <motion.div
+                    className={'bg-primary h-[4px]'}
+                    animate={{
+                        width: step > 2 ? '100%' : 0,
+                    }}
+                    transition={{
+                        duration: 0.2,
+                    }}
+                ></motion.div>
+            </div>
+            <div
+                className={cn(
+                    'flex h-14 w-14 items-center justify-center rounded-full text-xl text-white',
+                    `${step >= 3 ? 'bg-primary' : 'bg-black'}`,
+                )}
+            >
                 3
             </div>
-            <div className="w-12 border-[3px] border-black"></div>
-            <div className="flex h-14 w-14 items-center justify-center rounded-full bg-black text-xl text-white">
+            <div className={cn('h-[4px] w-12 bg-black')}>
+                <motion.div
+                    className={'bg-primary h-[4px]'}
+                    animate={{
+                        width: step > 3 ? '100%' : 0,
+                    }}
+                    transition={{
+                        duration: 0.2,
+                    }}
+                ></motion.div>
+            </div>
+            <div
+                className={cn(
+                    'flex h-14 w-14 items-center justify-center rounded-full text-xl text-white',
+                    `${step >= 4 ? 'bg-primary' : 'bg-black'}`,
+                )}
+            >
                 4
             </div>
         </div>

2e6aaaf37764fa0ba3abd3ef1fb015eacc0b4175
Yacine BOUKARI <steeveboukari9@gmail.com>
2023-12-21 00:26:18 +0100

    Update eslint config (#20)

--- src/components/tickets/create-ticket.tsx
+++ src/components/tickets/create-ticket.tsx
@@ -223,7 +223,7 @@ const CreateTicket: FunctionComponent<CreateTicketProps> = ({ eventStartingDate,
         if (!isModalOpened) {
             resetForm();
         }
-    }, [isModalOpened, resetForm]);
+    }, [isModalOpened]);
 
     return (
         <Fragment>

ffaf200299bcef21ff7be708ba0b0c0151ceb303
Yacine BOUKARI <steeveboukari9@gmail.com>
2024-01-21 14:05:37 +0100

    Add responsiveness to the 404 page (#62)

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,25 +7,25 @@ import { Button } from '@components/ui/buttons';
 
 const NotFoundPage: NextPage = () => {
   return (
-      <div className="flex flex-col w-screen h-screen">
+      <>
           <Header/>
-          <section className="fixed bottom-0 left-0 flex flex-col md:flex-row justify-between items-center gap-5 px-20 w-full h-[85vh]">
-              <div className="flex flex-col items-center md:items-start gap-7 w-full md:w-1/2">
+          <main className="fixed bottom-0 left-0 flex flex-col md:flex-row justify-center items-center gap-5 w-screen h-[86vh] md:h-[85vh] mt-[15vh] overflow-hidden">
+              <div className="flex flex-col items-center md:items-start gap-7 w-full md:w-1/2 px-5 md:px-10 xl:px-20">
                   <p className="text-[7vw] leading-[7vw] font-bold">
                       <span className="text-[7vw] leading-[7vw]">O</span>
                       <span className="text-[7vw] leading-[7vw] text-primary">oo</span>
                       ps!
                   </p>
-                  <p className="text-lg leading-[3.5vh]">
+                  <p className="text-center lg:text-start lg:text-lg lg:leading-[3.5vh]">
                       This page could not load. This may be a maintenance or construction issue. Try reloading the page
                       later
                       but in the main time feel free to create the next big event.
                   </p>
                   <Link href="/" className="w-fit focus:outline-none">
-                      <Button className="h-14 px-10 font-normal hover:bg-primary">Back to Home page</Button>
+                      <Button className="px-10 py-4 md:px-16 rounded-full lg:text-lg font-normal hover:bg-primary">Back to Home page</Button>
                   </Link>
               </div>
-              <div className="relative w-full md:w-1/2 h-full">
+              <div className="relative w-full aspect-video md:w-1/2 md:h-full md:aspect-auto">
                   <Image
                       src="/images/404.svg"
                       alt="404"
@@ -34,8 +34,8 @@ const NotFoundPage: NextPage = () => {
                       priority={true}
                   />
               </div>
-          </section>
-      </div>
+          </main>
+      </>
   );
 };
 

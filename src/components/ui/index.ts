7f253e2678787f861918c6fd57366f091b4b989b
Yacine BOUKARI <steeveboukari9@gmail.com>
2024-02-01 22:54:27 +0100

    Update index.ts

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,5 +3,3 @@ export { default as Loader } from './loader';
 export { default as ComingSoon } from './coming-soon';
 
 export { default as ImageWithFallback } from './image-with-fallback';
-
-export { default as Map } from './map';

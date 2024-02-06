354cdcfa7a39bea361e10eed5974652623a8c08a
Yacine BOUKARI <steeveboukari9@gmail.com>
2024-02-07 00:18:08 +0100

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,3 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
 import NextAuth from 'next-auth';
 import type { JWT } from 'next-auth/jwt';
 import CredentialsProvider from 'next-auth/providers/credentials';
@@ -7,52 +6,50 @@ import { authAPI } from '@/lib/api/auth';
 import { Role } from '@/types/constants';
 import type { SignInPayload } from '@/types/auth';
 
-const authHandler = (request: NextApiRequest, response: NextApiResponse) => {
-    return NextAuth(request, response, {
-        secret: serverEnv.NEXTAUTH_SECRET,
-        useSecureCookies: !isDevelopment,
-        callbacks: {
-            signIn: async ({ user }) => {
-                return [Role.ADMIN, Role.ORGANIZER].includes(user.role);
-            },
-            jwt: async ({ token, user }) => {
-                if (user) {
-                    token.user = user as JWT['user'];
-                }
-                
-                return token;
-            },
-            session: async ({ session, token }) => {
-                session.user = token.user;
-                
-                return session;
-            },
+const authHandler = NextAuth({
+    secret: serverEnv.NEXTAUTH_SECRET,
+    useSecureCookies: !isDevelopment,
+    callbacks: {
+        signIn: async ({ user }) => {
+            return [Role.ADMIN, Role.ORGANIZER].includes(user.role);
         },
-        providers: [
-            CredentialsProvider({
-                id: 'credentials',
-                name: 'Credentials',
-                type: 'credentials',
-                credentials: {
-                    email: { label: 'Email', type: 'email' },
-                    password: { label: 'Password', type: 'password' },
-                },
-                authorize: async (credentials?: SignInPayload) => {
-                    if (!credentials) {
-                        return null;
-                    }
-
-                    const signInResponse = await authAPI.signIn(credentials);
-                    
-                    if (!signInResponse.data.success) {
-                        return null
-                    }
-                    
-                    return signInResponse.data.data;
+        jwt: async ({ token, user }) => {
+            if (user) {
+                token.user = user as JWT['user'];
+            }
+            
+            return token;
+        },
+        session: async ({ session, token }) => {
+            session.user = token.user;
+            
+            return session;
+        },
+    },
+    providers: [
+        CredentialsProvider({
+            id: 'credentials',
+            name: 'Credentials',
+            type: 'credentials',
+            credentials: {
+                email: { label: 'Email', type: 'email' },
+                password: { label: 'Password', type: 'password' },
+            },
+            authorize: async (credentials?: SignInPayload) => {
+                if (!credentials) {
+                    return null;
                 }
-            }),
-        ],
-    });
-}
+                
+                const signInResponse = await authAPI.signIn(credentials);
+                
+                if (!signInResponse.data.success) {
+                    return null
+                }
+                
+                return signInResponse.data.data;
+            }
+        }),
+    ],
+});
 
 export { authHandler as GET, authHandler as POST };
